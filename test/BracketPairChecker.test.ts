e33ddd538d516c17815302be02029d81975d912c
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2020-11-07 15:02:26 +0100

    Added some test cases

--- test/BracketPairChecker.test.ts
+++ test/BracketPairChecker.test.ts
@@ -3,16 +3,37 @@ import { BracketPairChecker } from "../src/BracketPairChecker";
 
 export class BracketPairCheckerTest {
 
-	@Test("It should return true")
+	@Test("Correct case from the statement")
 	public shouldReturnTrue() {
 		let instance = new BracketPairChecker("[()]{}{[()()]()}");
 
 		Expect(instance.CheckBracketPairs()).toBe(true);
 	}
 
-	@Test("It should return false")
+	@Test("Incorrect case from the statement")
 	public shouldReturnFalse() {
 		let instance = new BracketPairChecker("[(])");
+
+		Expect(instance.CheckBracketPairs()).toBe(false);
+	}
+
+	@Test("Opening character at the end of string")
+	public shouldReturnFalseDueToNonEmptyStackAtTheEnd() {
+		let instance = new BracketPairChecker("[(])(");
+
+		Expect(instance.CheckBracketPairs()).toBe(false);
+	}
+
+	@Test("Closing character when stack is empty")
+	public shouldReturnFalseDueToEmptyStack() {
+		let instance = new BracketPairChecker("[{}]())");
+
+		Expect(instance.CheckBracketPairs()).toBe(false);
+	}
+
+	@Test("Closing character as first of the string")
+	public shouldReturnFalseDueToClosingCharAsFirst() {
+		let instance = new BracketPairChecker("}()[[{}]]()");
 
 		Expect(instance.CheckBracketPairs()).toBe(false);
 	}
